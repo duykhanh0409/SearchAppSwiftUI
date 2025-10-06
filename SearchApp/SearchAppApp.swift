a6d5fb78d3ad33a3a11b9fc6bf67759bd5a068b5
KhanhNguyen <duykhanh.nguyen@unifiedpost.com>
2025-10-06 22:39:24 +0700

    first commit

---
+++ SearchApp/SearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAppApp.swift
+//  SearchApp
+//
+//  Created by Khanh Nguyen on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SearchAppTests/SearchAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAppTests.swift
+//  SearchAppTests
+//
+//  Created by Khanh Nguyen on 6/10/25.
+//
+
+import Testing
+@testable import SearchApp
+
+struct SearchAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
